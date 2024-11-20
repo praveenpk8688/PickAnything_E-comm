51f2d1199dc47bcedbe7cdb22de8d3f2f64f4d43
PRAVEEN PASUPULETI <135953883+praveenpk8688@users.noreply.github.com>
2024-11-20 11:07:00 +0530

    fourth e-comm4 commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+// import "../Components/Css/BigDiscounts.css";
+import { Link } from 'react-router-dom';
+import Navbar from '../Components/Header/Navbar.jsx';
+import AutoPlay from '../Components/Header/Slider.jsx';
+import Colorcards from '../Components/Mainsection/Colorcards.jsx';
+import BigDiscounts from '../Components/Mainsection/BigDiscounts.jsx';
+import NewArrivels from '../Components/Mainsection/NewArrivels.jsx';
+import Bestsales from '../Components/Mainsection/Bestsales.jsx';
+import Footer from '../Components/Footer/Footer.jsx';
+
+
+
+
+
+
+
+function Home() {
+  return (
+    <>
+      <section className="home">
+       
+        <section className="navBar_main">
+          <Navbar />
+        </section>
+        <section className="hero_slick">
+          <AutoPlay />
+        </section>
+        <section className="colorCards_main">
+          <Colorcards />
+        </section>
+        <section className="Bigdisco">
+          <BigDiscounts />
+        </section>
+        <section className="NewARrivels">
+          <NewArrivels />
+        </section>
+        <section>
+          <Bestsales />
+        </section>
+        <section>
+        {/* <Product/> */}
+        </section>
+        <section>
+        <Footer/>
+        </section>
+      </section>
+    </>
+  );
+}
+
+export default Home
